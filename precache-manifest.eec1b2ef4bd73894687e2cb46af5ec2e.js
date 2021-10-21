b92a643ed78daacd169f0cbb95673f3b7486fb33
alimoghimi <alimoqimi97@gmail.com>
2021-10-21 05:22:13 -0700

    Updates

---
+++ precache-manifest.eec1b2ef4bd73894687e2cb46af5ec2e.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7056916aade5b27660dd597a368c6d01",
+    "url": "/tehrani/index.html"
+  },
+  {
+    "revision": "e55ac9ae6309314e3473",
+    "url": "/tehrani/static/css/2.8c4205ea.chunk.css"
+  },
+  {
+    "revision": "d2c7ad8ba027bbd1104f",
+    "url": "/tehrani/static/css/main.1d34b7c2.chunk.css"
+  },
+  {
+    "revision": "e55ac9ae6309314e3473",
+    "url": "/tehrani/static/js/2.4262c1c8.chunk.js"
+  },
+  {
+    "revision": "544eb1acafe931fc0332ddf129d299a3",
+    "url": "/tehrani/static/js/2.4262c1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c7ad8ba027bbd1104f",
+    "url": "/tehrani/static/js/main.b30624e9.chunk.js"
+  },
+  {
+    "revision": "cef26176e2eb88237b56",
+    "url": "/tehrani/static/js/runtime-main.fac8b90e.js"
+  },
+  {
+    "revision": "04b904e62a0d42f4bd864fb68b584421",
+    "url": "/tehrani/static/media/0_0.04b904e6.jpg"
+  },
+  {
+    "revision": "9c4ecd721043772bc61df2c466698759",
+    "url": "/tehrani/static/media/0_1.9c4ecd72.jpg"
+  },
+  {
+    "revision": "04b904e62a0d42f4bd864fb68b584421",
+    "url": "/tehrani/static/media/1.04b904e6.jpg"
+  },
+  {
+    "revision": "3518fad8d4f7406a7b1c8abcac52a106",
+    "url": "/tehrani/static/media/10.3518fad8.jpg"
+  },
+  {
+    "revision": "94d82768c6556e21224debb6b8efe67b",
+    "url": "/tehrani/static/media/1_0.94d82768.jpg"
+  },
+  {
+    "revision": "9c4ecd721043772bc61df2c466698759",
+    "url": "/tehrani/static/media/1_0.9c4ecd72.jpg"
+  },
+  {
+    "revision": "947271f6e426007d9c489eb55a6564c4",
+    "url": "/tehrani/static/media/1_1.947271f6.jpg"
+  },
+  {
+    "revision": "9c4ecd721043772bc61df2c466698759",
+    "url": "/tehrani/static/media/2.9c4ecd72.jpg"
+  },
+  {
+    "revision": "488f1f7d957643e963b73d9ef942722f",
+    "url": "/tehrani/static/media/2_0.488f1f7d.jpg"
+  },
+  {
+    "revision": "94d82768c6556e21224debb6b8efe67b",
+    "url": "/tehrani/static/media/2_0.94d82768.jpg"
+  },
+  {
+    "revision": "ba0011b32dd4cfc3ba71cf0cf0f514e7",
+    "url": "/tehrani/static/media/2_1.ba0011b3.jpg"
+  },
+  {
+    "revision": "94d82768c6556e21224debb6b8efe67b",
+    "url": "/tehrani/static/media/3.94d82768.jpg"
+  },
+  {
+    "revision": "947271f6e426007d9c489eb55a6564c4",
+    "url": "/tehrani/static/media/3_0.947271f6.jpg"
+  },
+  {
+    "revision": "be03b7b9fb240cd77af38e002182a744",
+    "url": "/tehrani/static/media/3_0.be03b7b9.jpg"
+  },
+  {
+    "revision": "8bf61ac41e25913770534beb50c2f713",
+    "url": "/tehrani/static/media/3_1.8bf61ac4.jpg"
+  },
+  {
+    "revision": "947271f6e426007d9c489eb55a6564c4",
+    "url": "/tehrani/static/media/4.947271f6.jpg"
+  },
+  {
+    "revision": "337eb93e0804d6738bc36d0e479266a0",
+    "url": "/tehrani/static/media/4_0.337eb93e.jpg"
+  },
+  {
+    "revision": "488f1f7d957643e963b73d9ef942722f",
+    "url": "/tehrani/static/media/4_0.488f1f7d.jpg"
+  },
+  {
+    "revision": "3518fad8d4f7406a7b1c8abcac52a106",
+    "url": "/tehrani/static/media/4_1.3518fad8.jpg"
+  },
+  {
+    "revision": "488f1f7d957643e963b73d9ef942722f",
+    "url": "/tehrani/static/media/5.488f1f7d.jpg"
+  },
+  {
+    "revision": "be03b7b9fb240cd77af38e002182a744",
+    "url": "/tehrani/static/media/5_0.be03b7b9.jpg"
+  },
+  {
+    "revision": "be03b7b9fb240cd77af38e002182a744",
+    "url": "/tehrani/static/media/6.be03b7b9.jpg"
+  },
+  {
+    "revision": "8bf61ac41e25913770534beb50c2f713",
+    "url": "/tehrani/static/media/6_0.8bf61ac4.jpg"
+  },
+  {
+    "revision": "8bf61ac41e25913770534beb50c2f713",
+    "url": "/tehrani/static/media/7.8bf61ac4.jpg"
+  },
+  {
+    "revision": "337eb93e0804d6738bc36d0e479266a0",
+    "url": "/tehrani/static/media/7_0.337eb93e.jpg"
+  },
+  {
+    "revision": "337eb93e0804d6738bc36d0e479266a0",
+    "url": "/tehrani/static/media/8.337eb93e.jpg"
+  },
+  {
+    "revision": "ba0011b32dd4cfc3ba71cf0cf0f514e7",
+    "url": "/tehrani/static/media/8_0.ba0011b3.jpg"
+  },
+  {
+    "revision": "ba0011b32dd4cfc3ba71cf0cf0f514e7",
+    "url": "/tehrani/static/media/9.ba0011b3.jpg"
+  },
+  {
+    "revision": "3518fad8d4f7406a7b1c8abcac52a106",
+    "url": "/tehrani/static/media/9_0.3518fad8.jpg"
+  },
+  {
+    "revision": "6b37574435abcb8c61d0a6773478b597",
+    "url": "/tehrani/static/media/bill2.6b375744.jpg"
+  },
+  {
+    "revision": "07cc5fdb47f878bead5a88d955bca12d",
+    "url": "/tehrani/static/media/bill3.07cc5fdb.jpg"
+  },
+  {
+    "revision": "2c7df951d5ee98d88f989e754d3a09e5",
+    "url": "/tehrani/static/media/bill4.2c7df951.jpg"
+  },
+  {
+    "revision": "cddfa84c6282dede4f2f75ca40dbacaa",
+    "url": "/tehrani/static/media/blog10.cddfa84c.jpg"
+  },
+  {
+    "revision": "c2d8115366f60f6527fe8cebc3454417",
+    "url": "/tehrani/static/media/boy.c2d81153.jpeg"
+  },
+  {
+    "revision": "da2e3cb3c0d7831052102340d479c081",
+    "url": "/tehrani/static/media/girl.da2e3cb3.jpeg"
+  },
+  {
+    "revision": "86628deb99866ff56d193a19b63510ba",
+    "url": "/tehrani/static/media/gnt.86628deb.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tehrani/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tehrani/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tehrani/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tehrani/static/media/slick.f97e3bbf.svg"
+  }
+]);
